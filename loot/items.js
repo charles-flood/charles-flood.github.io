7af03ee6866fc0c9e60ee531af6e9f9d795e99a4
unknown <fluddx@gmail.com>
2026-08-05 19:01:46 +0100

    added mini engine, some minor cleanup

--- loot/items.js
+++ loot/items.js
@@ -1,76 +1,76 @@
 //item bases
 const itemTypes = [
-        {
-            name: "Short Sword",
-            type: "Weapon",
-            minDamage: 2,
-            maxDamage: 6
-        },
-        {
-            name: "Long Sword",
-            type: "Weapon",
-            minDamage: 3,
-            maxDamage: 8
-        },
-        {
-            name: "Scimitar",
-            type: "Weapon",
-            minDamage: 5,
-            maxDamage: 15
-        },
-        {
-            name: "Bastard Sword",
-            type: "Weapon",
-            minDamage: 12,
-            maxDamage: 25
-        },
-        {
-            name: "Two Handed Sword",
-            type: "Weapon",
-            minDamage: 20,
-            maxDamage: 45
-        },
-        {
-            name: "Great Sword",
-            type: "Weapon",
-            minDamage: 40,
-            maxDamage: 65
-        },
-        //armors
-        {
-            name: "Quilted Armor",
-            type: "Armor",
-            minDefense: 10,
-            maxDefense: 15
-        },
-        {
-            name: "Leather Armor",
-            type: "Armor",
-            minDefense: 12,
-            maxDefense: 25
-        },
-        {
-            name: "Chain Mail",
-            type: "Armor",
-            minDefense: 20,
-            maxDefense: 40
-        },
-        {
-            name: "Plate Mail",
-            type: "Armor",
-            minDefense: 35,
-            maxDefense: 65
-        },
-        {
-            name: "BreastPlate",
-            type: "Armor",
-            minDefense: 60,
-            maxDefense: 90
-        },
-        {
-            name: "Full Plate",
-            type: "Armor",
-            minDefense: 80,
-            maxDefense: 135
-        }
+    {
+        name: "Short Sword",
+        type: "Weapon",
+        minDamage: 2,
+        maxDamage: 6
+    },
+    {
+        name: "Long Sword",
+        type: "Weapon",
+        minDamage: 3,
+        maxDamage: 8
+    },
+    {
+        name: "Scimitar",
+        type: "Weapon",
+        minDamage: 5,
+        maxDamage: 15
+    },
+    {
+        name: "Bastard Sword",
+        type: "Weapon",
+        minDamage: 12,
+        maxDamage: 25
+    },
+    {
+        name: "Two Handed Sword",
+        type: "Weapon",
+        minDamage: 20,
+        maxDamage: 45
+    },
+    {
+        name: "Great Sword",
+        type: "Weapon",
+        minDamage: 40,
+        maxDamage: 65
+    },
+    //armors
+    {
+        name: "Quilted Armor",
+        type: "Armor",
+        minDefense: 10,
+        maxDefense: 15
+    },
+    {
+        name: "Leather Armor",
+        type: "Armor",
+        minDefense: 12,
+        maxDefense: 25
+    },
+    {
+        name: "Chain Mail",
+        type: "Armor",
+        minDefense: 20,
+        maxDefense: 40
+    },
+    {
+        name: "Plate Mail",
+        type: "Armor",
+        minDefense: 35,
+        maxDefense: 65
+    },
+    {
+        name: "BreastPlate",
+        type: "Armor",
+        minDefense: 60,
+        maxDefense: 90
+    },
+    {
+        name: "Full Plate",
+        type: "Armor",
+        minDefense: 80,
+        maxDefense: 135
+    }
 ];
